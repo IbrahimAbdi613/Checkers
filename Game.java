b148da10dea18dd51c783eac2e55c39fa7c9825b
IbrahimAbdi613 <ab46467878@gmail.com>
2020-12-26 14:34:45 -0500

    change the positon of the Black and White peices so that they dont overlap

--- Game.java
+++ Game.java
@@ -8,19 +8,24 @@ public class Game {
         White = new checkerPiece[12];
         Black = new checkerPiece[12];
         int x = 0;
+        int x2 = 0;
         int y = -1;
         for (int i = 0; i < 12; i++) {
             if ((i % 4) == 0) {
                 y++;
                 if (board.Board[y][0] == 1) {
                     x = 0;
+                    x2 = 1;
                 }
-                else
+                else {
                     x = 1;
+                    x2 = 0;
+                }
             }
             White[i] = new checkerPiece(x, y, "White");
-            Black[i] = new checkerPiece(x, y, "Black");
+            Black[i] = new checkerPiece(x2, 7 - y, "Black");
             x = x + 2;
+            x2 = x2 + 2;
         }
     }
     
--- checkerPiece.java
+++ checkerPiece.java
@@ -2,13 +2,19 @@ public class checkerPiece {
     int x;
     int y;
     String colour;
+    Boolean King;
 
     public checkerPiece(int x, int y, String colour) {
         this.x = x;
         this.y = y;
         this.colour = colour;
+        King = false;
     }
+
+    public void moveRight() {
     
+    }
+
 
 
 
